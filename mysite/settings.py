865de958da45e2131707f89b3e30dad156cc1a35
Dan Sapp <dan.sapp@storable.com>
2024-11-27 15:43:51 -0600

    heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('notebooks/<str:notebook_name>/', views.view_notebook, name='view_notebook'),
 ]
+if True:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
