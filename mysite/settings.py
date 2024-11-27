9ac612f2524a0a0cd45de47bcd33ce78a80ac403
Dan Sapp <dan.sapp@storable.com>
2024-11-27 14:25:48 -0600

    remove files

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,26 +1,18 @@
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
 SECRET_KEY = 'django-insecure-04tk+vec^t*ef+1t$%nmn3j5#h8la5xw=lmiaredtun*fj@5j2'
 DEBUG = True
 ALLOWED_HOSTS = []
 
 # Application definition
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
     'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
@@ -42,30 +34,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
-
-# Database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# Password validation
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
--- mysite/views.py
+++ mysite/views.py
@@ -11,17 +11,6 @@ def view_notebook(request, notebook_name):
         return render(request, 'notebook.html', {'notebook_html': notebook_html})
     else:
         return render(request, '404.html', {'message': 'Notebook not found'})
-
-def serve_notebook(request, notebook_name):
-    # Construct the path to the notebook HTML
-    template_path = f'notebooks/{notebook_name}.html'
-
-    # Check if the file exists
-    if os.path.exists(os.path.join('myapp/templates/', template_path)):
-        return render(request, template_path)
-    else:
-        # Handle the case where the notebook doesn't exist
-        return render(request, '404.html', {'message': 'Notebook not found'})
       
 def home(request):
   return render(request, 'home.html')
