3732d139ca5ae9123dad0552516df8efef376d55
Dan Sapp <dan.sapp@storable.com>
2024-11-30 17:54:30 -0600

    admin, gen 1-4 pokemon

--- pokemon/admin.py
+++ pokemon/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Pokemon
 
-# Register your models here.
+@admin.register(Pokemon)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'types', 'gen_of_first_appearance')
