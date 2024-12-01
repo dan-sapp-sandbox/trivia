f8ea6529ae465501259220a3c95d2358b8094e36
Dan Sapp <dan.sapp@storable.com>
2024-11-30 18:45:07 -0600

    test

--- mysite/views.py
+++ mysite/views.py
@@ -5,7 +5,7 @@ import ast
 from django.shortcuts import render, redirect
 from django.conf import settings
 from comics.views import generate_superhero_appearance_question, generate_superhero_power_question
-# from movies.views import generate_movie_has_actor_question, generate_actor_in_movie_question
+from movies.views import generate_movie_has_actor_question
 # from movies.views import generate_movie_has_actor_question, generate_actor_in_movie_question, generate_year_of_movie_question
 # from music.views import generate_artist_of_song
 # from pokemon.views import generate_pokemon_of_type
@@ -27,11 +27,11 @@ def load_trivia_questions(selected_options, num_questions):
             generate_superhero_power_question() for _ in range(num_questions)
         ]
         
-    # movie_has_actor_questions = []
-    # if 'movies' in selected_options:
-    #     actor_in_movie_questions = [
-    #         generate_movie_has_actor_question() for _ in range(num_questions)
-    #     ]
+    movie_has_actor_questions = []
+    if 'movies' in selected_options:
+        actor_in_movie_questions = [
+            generate_movie_has_actor_question() for _ in range(num_questions)
+        ]
         
     # actor_in_movie_questions = []
     # if 'movies' in selected_options:
@@ -66,7 +66,7 @@ def load_trivia_questions(selected_options, num_questions):
     question_lists = [
         superhero_appearance_questions,
         superhero_power_questions,
-        # movie_has_actor_questions,
+        movie_has_actor_questions,
         # actor_in_movie_questions,
         # year_of_movie_questions,
         # artist_of_song_questions,
