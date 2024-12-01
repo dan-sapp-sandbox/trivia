64a62a407b0fc134163d94cb26370725a1390b26
Dan Sapp <dan.sapp@storable.com>
2024-11-30 18:35:41 -0600

    test

--- mysite/views.py
+++ mysite/views.py
@@ -4,10 +4,11 @@ import os
 import ast
 from django.shortcuts import render, redirect
 from django.conf import settings
-from comics.views import generate_superhero_appearance_question, generate_superhero_power_question
-from movies.views import generate_movie_has_actor_question, generate_actor_in_movie_question
-from music.views import generate_artist_of_song
-from pokemon.views import generate_pokemon_of_type
+from comics.views import generate_superhero_appearance_question
+# from comics.views import generate_superhero_appearance_question, generate_superhero_power_question
+# from movies.views import generate_movie_has_actor_question, generate_actor_in_movie_question
+# from music.views import generate_artist_of_song
+# from pokemon.views import generate_pokemon_of_type
 # from geography.views import generate_landmark_at_coord_question
       
 def home(request):
@@ -19,22 +20,22 @@ def load_trivia_questions(selected_options, num_questions):
         superhero_appearance_questions = [
             generate_superhero_appearance_question() for _ in range(num_questions)
         ]
-    superhero_power_questions = []
-    if 'comics' in selected_options:
-        superhero_power_questions = [
-            generate_superhero_power_question() for _ in range(num_questions)
-        ]
-    movie_has_actor_questions = []
-    if 'movies' in selected_options:
-        actor_in_movie_questions = [
-            generate_movie_has_actor_question() for _ in range(num_questions)
-        ]
+    # superhero_power_questions = []
+    # if 'comics' in selected_options:
+    #     superhero_power_questions = [
+    #         generate_superhero_power_question() for _ in range(num_questions)
+    #     ]
+    # movie_has_actor_questions = []
+    # if 'movies' in selected_options:
+    #     actor_in_movie_questions = [
+    #         generate_movie_has_actor_question() for _ in range(num_questions)
+    #     ]
         
-    actor_in_movie_questions = []
-    if 'movies' in selected_options:
-        actor_in_movie_questions = [
-            generate_actor_in_movie_question() for _ in range(num_questions)
-        ]
+    # actor_in_movie_questions = []
+    # if 'movies' in selected_options:
+    #     actor_in_movie_questions = [
+    #         generate_actor_in_movie_question() for _ in range(num_questions)
+    #     ]
         
     # year_of_movie_questions = []
     # if 'movies' in selected_options:
@@ -42,17 +43,17 @@ def load_trivia_questions(selected_options, num_questions):
     #         generate_year_of_movie_question() for _ in range(num_questions)
     #     ]
         
-    artist_of_song_questions = []
-    if 'music' in selected_options:
-        artist_of_song_questions = [
-            generate_artist_of_song() for _ in range(num_questions)
-        ]
+    # artist_of_song_questions = []
+    # if 'music' in selected_options:
+    #     artist_of_song_questions = [
+    #         generate_artist_of_song() for _ in range(num_questions)
+    #     ]
         
-    pokemon_of_type_questions = []
-    if 'pokemon' in selected_options:
-        pokemon_of_type_questions = [
-            generate_pokemon_of_type() for _ in range(num_questions)
-        ]
+    # pokemon_of_type_questions = []
+    # if 'pokemon' in selected_options:
+    #     pokemon_of_type_questions = [
+    #         generate_pokemon_of_type() for _ in range(num_questions)
+    #     ]
         
     # landmark_at_coord_questions = []
     # if 'geography' in selected_options:
@@ -62,11 +63,11 @@ def load_trivia_questions(selected_options, num_questions):
         
     question_lists = [
         superhero_appearance_questions,
-        superhero_power_questions,
-        movie_has_actor_questions,
-        actor_in_movie_questions,
-        artist_of_song_questions,
-        pokemon_of_type_questions,
+        # superhero_power_questions,
+        # movie_has_actor_questions,
+        # actor_in_movie_questions,
+        # artist_of_song_questions,
+        # pokemon_of_type_questions,
     ]
     questions = [question for sublist in question_lists for question in sublist]
     return questions
