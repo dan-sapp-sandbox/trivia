50dd941539757ab9c421ca6b53b5bb2b1445de94
Dan Sapp <dan.sapp@storable.com>
2024-11-30 18:25:14 -0600

    fix build

--- mysite/views.py
+++ mysite/views.py
@@ -5,7 +5,7 @@ import ast
 from django.shortcuts import render, redirect
 from django.conf import settings
 from comics.views import generate_superhero_appearance_question, generate_superhero_power_question
-from movies.views import generate_movie_has_actor_question, generate_actor_in_movie_question, generate_year_of_movie_question
+from movies.views import generate_movie_has_actor_question, generate_actor_in_movie_question
 from music.views import generate_artist_of_song
 from pokemon.views import generate_pokemon_of_type
 # from geography.views import generate_landmark_at_coord_question
@@ -36,11 +36,11 @@ def load_trivia_questions(selected_options, num_questions):
             generate_actor_in_movie_question() for _ in range(num_questions)
         ]
         
-    year_of_movie_questions = []
-    if 'movies' in selected_options:
-        year_of_movie_questions = [
-            generate_year_of_movie_question() for _ in range(num_questions)
-        ]
+    # year_of_movie_questions = []
+    # if 'movies' in selected_options:
+    #     year_of_movie_questions = [
+    #         generate_year_of_movie_question() for _ in range(num_questions)
+    #     ]
         
     artist_of_song_questions = []
     if 'music' in selected_options:
